6737dd15e24ce62ee8c2764bc7b2828fef19160a
ruanjianwei <ruanjianwei@wecash.net>
2019-05-31 18:15:34 +0800

    Updates

---
+++ precache-manifest.78597858d6d8c60f4e61f0586e9eedd5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c7f42129dd637a4bb94db2fa9be7193",
+    "url": "/react_tutorial/index.html"
+  },
+  {
+    "revision": "f40eec09d1d9165d3660",
+    "url": "/react_tutorial/static/css/main.682ca21c.chunk.css"
+  },
+  {
+    "revision": "3a9e33913cab0720716d",
+    "url": "/react_tutorial/static/js/2.86a2265c.chunk.js"
+  },
+  {
+    "revision": "f40eec09d1d9165d3660",
+    "url": "/react_tutorial/static/js/main.c2ac2868.chunk.js"
+  },
+  {
+    "revision": "263b86900b78269fabd7",
+    "url": "/react_tutorial/static/js/runtime~main.46666d96.js"
+  }
+]);
