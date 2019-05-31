5a81671b9a3c7a523cefe5318e52fa357227f0d1
ruanjianwei <ruanjianwei@wecash.net>
2019-05-31 16:14:16 +0800

    Updates

---
+++ precache-manifest.2dd94a84e4471ab1bc0f65e022290349.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aca2f4dcd342c499c15ca67d082e690",
+    "url": "/wooer79/react_tutorial/index.html"
+  },
+  {
+    "revision": "f40eec09d1d9165d3660",
+    "url": "/wooer79/react_tutorial/static/css/main.682ca21c.chunk.css"
+  },
+  {
+    "revision": "3a9e33913cab0720716d",
+    "url": "/wooer79/react_tutorial/static/js/2.86a2265c.chunk.js"
+  },
+  {
+    "revision": "f40eec09d1d9165d3660",
+    "url": "/wooer79/react_tutorial/static/js/main.c2ac2868.chunk.js"
+  },
+  {
+    "revision": "4627555b28a5ec998f7b",
+    "url": "/wooer79/react_tutorial/static/js/runtime~main.2d722f02.js"
+  }
+]);
